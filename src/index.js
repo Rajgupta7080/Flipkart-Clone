b0c7880262d23688fa1eee4f37b00630cc9aeed5
bkabhi <bkabhijeet9801@gmail.com>
2022-10-15 12:03:51 +0530

    op

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { store } from './Redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider>
+    <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Provider>
   </ChakraProvider>
 );
